2d78402881b3f3664a041bb969f47191cab5998a
s1mple-o <simple12308@sina.com>
2021-08-30 23:19:12 +0800

    部分页面

---
+++ js/base.js
@@ -0,0 +1,18 @@
+﻿(function(){
+
+	//新tabs切换
+	function tabs(handles,cons){
+		$(handles).each(function(num){
+	    	$(this).click(function(){    		
+	    		$(handles).removeClass("hover");
+	    		$(this).addClass("hover");
+	    		$(cons).removeClass("show");
+	    		$($(cons)[num]).addClass("show");
+	    	});
+	    });
+	}
+
+	window.tabs = tabs;
+
+})();
+
